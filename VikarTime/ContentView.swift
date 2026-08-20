d43b31e14091999004cf8bca3779e46e67781909
Taisan Tamang <tisb4u.tt@gmail.com>
2026-08-20 22:50:57 +0200

    Set up initial VikarTime app

--- VikarTime/ContentView.swift
+++ VikarTime/ContentView.swift
@@ -1,19 +1,19 @@
-//
-//  ContentView.swift
-//  VikarTime
-//
-//  Created by Kabita Limbu on 20/08/2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+
+            Image(systemName: "clock.fill")
+                .font(.system(size: 60))
+
+            Text("VikarTime")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Manage your shifts and working hours")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
         }
         .padding()
     }
